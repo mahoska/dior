6fc5b84ba8f02660dba04603563033e97edb3e52
mahoska <annamakhovskayawm@gmail.com>
2024-04-24 18:58:33 +0300

    header and slider done

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    effect: 'fade',
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
